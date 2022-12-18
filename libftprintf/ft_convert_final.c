706cc88d603cdf93676ae6d0cf671d78b012f290
mtoof <mojtaba.toof@gmail.com>
2022-12-18 23:43:57 +0200

    final

--- libftprintf/ft_convert_final.c
+++ libftprintf/ft_convert_final.c
@@ -3,52 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   ft_convert.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
+/*   By: sepehr <sepehr@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 18:36:11 by mtoof             #+#    #+#             */
-/*   Updated: 2022/12/14 16:21:46 by mtoof            ###   ########.fr       */
+/*   Updated: 2022/12/18 23:41:35 by sepehr           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int    nlen(unsigned long n, int base)
+static int	nlen(unsigned long n, int base, char sign)
 {
-    int    i;
+	int	i;
 
-    i = 0;
-    if (n == 0)
-        return (1);
-    while (n)
-    {
-        n = (n / base);
-        i++;
-    }
-    return (i);
+	i = 0;
+	if (n == 0)
+		return (1);
+	if (sign == 1)
+	{
+		if ((int)n < 0)
+		{
+		n = (int)n * -1;
+		i++;
+		}
+	}
+	while (n)
+	{
+		n = (n / base);
+		i++;
+	}
+	return (i);
 }
 
-int    ft_convert(unsigned long num, int base, char sign)
+int	ft_convert(unsigned long num, int base, char sign)
 {
-    int    counter;
-    int digit_counter;
-    char nums[16] = "0123456789ABCDEF";
-    char tmp;
-    char *ptr;
+	int		counter;
+	int		digit_counter;
+	char	*nums;
+	char	*ptr;
 
-    digit_counter = 0;
-    counter = 0;
-    digit_counter = nlen(num, base);
-    ptr = (char *)malloc(sizeof(char)* digit_counter + 1);
-    ptr[digit_counter--] = '\0';
-    while (num != 0)
-    {
-      tmp = nums[num % base];
-      if (tmp > '9')
-        ptr[digit_counter--] = (tmp + sign);
-      else
-        ptr[digit_counter--] = tmp;
-      num /= base;
-    }
-    counter += ft_putstr(ptr);
-    return (counter);
+	nums = "0123456789ABCDEF";
+	counter = 0;
+	digit_counter = nlen(num, base, sign);
+	ptr = (char *)malloc(sizeof(char) * digit_counter + 1);
+	if (!ptr)
+		return (0);
+	ptr[digit_counter--] = '\0';
+	if (num == 0)
+		ptr[digit_counter--] = '0';
+	while (num)
+	{
+		if (nums[num % base] > '9')
+			ptr[digit_counter--] = (nums[num % base] + sign);
+		else
+			ptr[digit_counter--] = nums[num % base];
+		num /= base;
+	}
+	counter += ft_putstr(ptr);
+	free(ptr);
+	return (counter);
 }
