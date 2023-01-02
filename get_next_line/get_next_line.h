40f5f4791388e6e93de633667aaaf0d074143693
mtoof <112095479+mtoof@users.noreply.github.com>
2023-01-02 14:37:14 +0200

    Update get_next_line.h

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -14,14 +14,13 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
-# include <sys/types.h>
-# include <sys/uio.h>
+// # include <sys/types.h>
+// # include <sys/uio.h>
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 8
 # endif
-
 
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
